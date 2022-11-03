a4fb1217c72ee3482dc8c1182f0a02819096cf50
italobarzola <italo.barzola@links.com.ec>
2022-11-02 20:51:36 -0500

    Create scafolding authentication

---
+++ src/Contexts/Authentication/domain/Authentication.ts
@@ -0,0 +1,10 @@
+import { AggregateRoot } from "../../Shared/domain/AggregateRoot";
+
+export class Authentication extends AggregateRoot {
+
+    constructor() {
+        super();
+    }
+
+    toPrimitives() { }
+}
