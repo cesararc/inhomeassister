5f52cc6aac962563a69cfbe868d99adb80f3a61b
italobarzola <italo.barzola@links.com.ec>
2022-11-10 22:21:31 -0500

    Profile service provider

--- src/Apps/ServiceProvider/routes/serviceProvider.route.ts
+++ src/Apps/ServiceProvider/routes/serviceProvider.route.ts
@@ -4,5 +4,8 @@ import container from '../../dependency-injection';
 export const register = (router: Router) => {
     const serviceProviderCreateController = container.get('ServiceProvider.ServiceProviderCreateController');
 
+    const serviceProviderProfileController = container.get('ServiceProvider.ServiceProviderProfileController');
+
     router.post("/api/service_provider", (req: Request, res: Response) => serviceProviderCreateController.run(req, res));
+    router.get("/api/service_provider/profile/:uid", (req: Request, res: Response) => serviceProviderProfileController.run(req, res));
 }
