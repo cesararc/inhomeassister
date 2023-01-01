73313cc0989a6fd1cf2d6f58ea4375d41fb1676f
italobarzola <italo.barzola@links.com.ec>
2023-01-01 16:43:52 -0500

    Admin add reward point

--- src/Apps/middleware/isAuthenticated.ts
+++ src/Apps/middleware/isAuthenticated.ts
@@ -3,18 +3,19 @@ import httpStatus from 'http-status';
 import { auth } from '../database';
 
 const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
+
     const { authorization } = req.headers;
 
-    if (!authorization) res.sendStatus(httpStatus.UNAUTHORIZED);
-
-    if (!authorization.startsWith('Bearer')) res.sendStatus(httpStatus.UNAUTHORIZED);
-
-    const split = authorization.split('Bearer ')
-    if (split.length !== 2) res.sendStatus(httpStatus.UNAUTHORIZED);
-
-    const token = split[1];
-
     try {
+        if (!authorization) res.sendStatus(httpStatus.UNAUTHORIZED);
+
+        if (!authorization.startsWith('Bearer')) res.sendStatus(httpStatus.UNAUTHORIZED);
+
+        const split = authorization.split('Bearer ')
+        if (split.length !== 2) res.sendStatus(httpStatus.UNAUTHORIZED);
+
+        const token = split[1];
+
         const decodedToken = await auth.verifyIdToken(token);
         res.locals = { ...res.locals, uid: decodedToken.uid, role: decodedToken.role, email: decodedToken.email };
 
--- src/Apps/middleware/isAuthorized.ts
+++ src/Apps/middleware/isAuthorized.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import httpStatus from 'http-status';
 
-export function isAuthorized(opts: { hasRole: Array<'customer' | 'service_provider' | 'seller'> }) {
+export function isAuthorized(opts: { hasRole: Array<'customer' | 'service_provider' | 'seller' | "admin"> }) {
     return (req: Request, res: Response, next: NextFunction) => {
         const { role } = res.locals;
 
