000950727fcaec45fd6f61c0ecf8f7d0f55f9fca
italobarzola <italo.barzola@links.com.ec>
2023-01-18 22:18:26 -0500

    Add mail collection

--- src/Contexts/Notifications/infrastructure/EmailSenderService.ts
+++ src/Contexts/Notifications/infrastructure/EmailSenderService.ts
@@ -1,9 +1,24 @@
+import firestore from '../../../Apps/database';
 import { Email } from "../domain/Email";
 import { EmailRepository } from "../domain/EmailRepository";
 
 export class EmailSenderService implements EmailRepository {
 
+    protected collection() {
+        return firestore.collection("mail");
+    }
+
     async send(email: Email): Promise<void> {
+        const doc = {
+            to: email.to.value,
+            message: {
+                subject: email.subject.value,
+                text: email.body.value
+            },
+        }
+
+        const reference = await this.collection().add(doc);
+        console.log(reference);
         console.log('Sending email to ', email);
     }
 }
