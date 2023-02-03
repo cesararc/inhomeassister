f12692b662749cb949f44a4fb086b0fb5eb11dbe
italobarzola <italo.barzola@links.com.ec>
2023-02-02 23:02:06 -0500

    Actions protected user record

--- src/Apps/UserRecord/routes/userRecord.route.ts
+++ src/Apps/UserRecord/routes/userRecord.route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import container from '../../dependency-injection';
 import { isAuthenticated } from '../../middleware/isAuthenticated';
+import { isAuthorized } from '../../middleware/isAuthorized';
 
 export const register = (router: Router) => {
     const UserRecordProfileController = container.get('UserRecord.UserRecordProfileController');
@@ -8,8 +9,17 @@ export const register = (router: Router) => {
     const UserRecordDisableController = container.get('UserRecord.UserRecordDisableController');
     const UserRecordEnableController = container.get('UserRecord.UserRecordEnableController');
 
-    router.get("/api/user-record/profile/:uid", isAuthenticated, (...params) => UserRecordProfileController.run(...params));
-    router.post("/api/user-record/enable/:uid", isAuthenticated, (...params) => UserRecordEnableController.run(...params));
-    router.post("/api/user-record/disable/:uid", isAuthenticated, (...params) => UserRecordDisableController.run(...params));
+    router.get("/api/user-record/profile/:uid",
+        isAuthenticated,
+        isAuthorized({ hasRole: ["admin", "customer", "seller", "service_provider"] }),
+        (...params) => UserRecordProfileController.run(...params));
+    router.post("/api/user-record/enable/:uid",
+        isAuthenticated,
+        isAuthorized({ hasRole: ["admin"] }),
+        (...params) => UserRecordEnableController.run(...params));
+    router.post("/api/user-record/disable/:uid",
+        isAuthenticated,
+        isAuthorized({ hasRole: ["admin"] }),
+        (...params) => UserRecordDisableController.run(...params));
     router.post("/api/user-record/reset-password/:email", (...params) => UserRecordResetPasswordController.run(...params));
 }
