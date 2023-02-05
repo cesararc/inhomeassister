6db01a03dc1d23bc941f031280ef5b295b1fd694
italobarzola <italo.barzola@links.com.ec>
2023-02-04 21:06:19 -0500

    Use case atomic customer

--- src/Apps/Seller/controllers/SellerCreateController.ts
+++ src/Apps/Seller/controllers/SellerCreateController.ts
@@ -2,9 +2,7 @@ import httpStatus from 'http-status';
 import { Controller } from '../../controller/Controller';
 import { Response, Request } from 'express';
 import { CommandBus } from '../../../Contexts/Shared/domain/CommandBus';
-import { UserRecordCreateCommand } from '../../../Contexts/UserRecord/application/Create/UserRecordCreateCommand';
 import { SellerCreateCommand } from '../../../Contexts/Seller/application/create/SellerCreateCommand';
-import { UserRecordDeleteCommand } from '../../../Contexts/UserRecord/application/Delete/UserRecordDeleteCommand';
 
 export class SellerCreateController implements Controller {
 
@@ -14,37 +12,28 @@ export class SellerCreateController implements Controller {
         const uid = req.body.uid;
         const displayName = req.body.displayName;
         const email = req.body.email;
-        const phone = req.body.phone;
+        const phoneNumber = req.body.phone;
         const password = req.body.password;
         const address = req.body.address;
         const dni = req.body.dni;
         const claim = req.body.claim;
 
         try {
-            const sellerCreateCommand = new SellerCreateCommand({
+            const command = new SellerCreateCommand({
                 uid,
+                displayName,
+                email,
+                password,
+                phoneNumber,
+                claim,
                 address,
                 dni
             });
 
-            const userRecordCreateCommand = new UserRecordCreateCommand({
-                displayName,
-                email,
-                password,
-                phone,
-                claim,
-                uid
-            });
-
-            await this.commandBus.dispatch(userRecordCreateCommand);
-
-            await this.commandBus.dispatch(sellerCreateCommand);
+            await this.commandBus.dispatch(command);
 
             res.status(httpStatus.CREATED).send();
         } catch (error) {
-            const rollback = new UserRecordDeleteCommand(uid);
-
-            this.commandBus.dispatch(rollback);
 
             res.status(httpStatus.BAD_REQUEST).send(error.message);
         }
--- src/Contexts/Seller/application/create/SellerCreate.ts
+++ src/Contexts/Seller/application/create/SellerCreate.ts
@@ -4,15 +4,42 @@ import { SellerAddress } from '../../domain/SellerAddress';
 import { SellerDni } from '../../domain/SellerDni';
 import { Seller } from '../../domain/Seller';
 import { EventBus } from '../../../Shared/domain/EventBus';
+import { UserRecordRepository } from '../../../UserRecord/domain/UserRecordRepository';
+import { UserRecordDisplayName } from '../../../UserRecord/domain/UserRecordDisplayName';
+import { UserRecordPhone } from '../../../UserRecord/domain/UserRecordPhone';
+import { UserRecordEmail } from '../../../UserRecord/domain/UserRecordEmail';
+import { UserRecordPassword } from '../../../UserRecord/domain/UserRecordPassword';
+import { UserRecordClaim } from '../../../UserRecord/domain/UserRecordClaim';
+import { UserRecord } from '../../../UserRecord/domain/UserRecord';
 
 export class SellerCreate {
-    constructor(private repository: SellerRepository, private eventBus: EventBus) { }
+    constructor(
+        private sellerRepository: SellerRepository,
+        private userRecordRepository: UserRecordRepository,
+        private eventBus: EventBus) { }
 
-    async run(uid: SellerUid, address: SellerAddress, dni: SellerDni): Promise<void> {
-        const serviceProvider = Seller.create(uid, address, dni);
+    async run(
+        uid: SellerUid,
+        displayName: UserRecordDisplayName,
+        phoneNumber: UserRecordPhone,
+        email: UserRecordEmail,
+        password: UserRecordPassword,
+        claim: UserRecordClaim,
+        address: SellerAddress,
+        dni: SellerDni): Promise<void> {
 
-        await this.repository.create(serviceProvider);
+        try {
+            const userRecord = UserRecord.create(uid, displayName, phoneNumber, email, password, claim);
 
-        await this.eventBus.publish(serviceProvider.pullDomainEvents());
+            await this.userRecordRepository.create(userRecord);
+
+            const serviceProvider = Seller.create(uid, address, dni);
+
+            await this.sellerRepository.create(serviceProvider);
+
+            await this.eventBus.publish(serviceProvider.pullDomainEvents());
+        } catch (error) {
+            await this.userRecordRepository.delete(uid);
+        }
     }
 }
