67516b30be4e116562442a564ba65f8ed814fa4d
italobarzola <italo.barzola@links.com.ec>
2023-02-04 20:50:29 -0500

    Use case atomic customer

--- src/Contexts/Customer/application/create/CustomerCreateCommand.ts
+++ src/Contexts/Customer/application/create/CustomerCreateCommand.ts
@@ -2,13 +2,32 @@ import { Command } from "../../../Shared/domain/Command";
 
 export class CustomerCreateCommand extends Command {
     uid: string;
+    displayName: string;
+    phoneNumber: string;
+    email: string;
+    password: string;
+    claim: string;
     address: string;
     birthday: string;
     dni: string;
 
-    constructor({ uid, address, birthday, dni }: { uid: string, address: string, birthday: string; dni: string; }) {
+    constructor({ uid, displayName, phoneNumber, email, password, claim, address, birthday, dni }: {
+        uid: string; displayName: string;
+        phoneNumber: string;
+        email: string;
+        claim: string;
+        password: string;
+        address: string;
+        birthday: string;
+        dni: string;
+    }) {
         super();
         this.uid = uid;
+        this.displayName = displayName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+        this.claim = claim;
         this.address = address;
         this.birthday = birthday;
         this.dni = dni;
